a5bc11ac5d10445502506a1ab2e462e063ac0bd6
Digbijayinee Clara <digbijayineeclara@Digbijayinees-MacBook-Pro.local>
2024-08-07 14:11:41 +0530

    stackguardian-role-terraform-changes-2st-commit

--- stackguardian_role/variables.tf
+++ stackguardian_role/variables.tf
@@ -2,3 +2,11 @@ variable "roleName" {
   type = string
   default = "StackGuardian-role-example"
 }
+variable "api_key" {
+  type = string
+  default = "value"
+}
+variable "org_name" {
+  type = string
+  default = "StackGuardian"
+}
