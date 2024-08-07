38781ea93d3a03f2bf716b414fcb0010473177b7
Digbijayinee Clara <digbijayineeclara@Digbijayinees-MacBook-Pro.local>
2024-08-07 16:30:14 +0530

    stackguardian-workflow-group-terraform-changes-3rd-commit

--- stackguardian_workflow_group/main.tf
+++ stackguardian_workflow_group/main.tf
@@ -1,6 +1,6 @@
-resource "stackguardian_workflow_group" "TPS-Example-WorkflowGroup" {
+resource "stackguardian_workflow_group" "Example-WorkflowGroup" {
   data = jsonencode({
-    "ResourceName" : "TPS-Example",
+    "ResourceName" : var.ResourceName,
     "Description" : "Example of terraform-provider-stackguardian for WorkflowGroup",
     "Tags" : ["tf-provider-example"],
     "IsActive" : 1,
--- stackguardian_workflow_group/variables.tf
+++ stackguardian_workflow_group/variables.tf
@@ -1,10 +1,12 @@
 variable "api_key" {
   type = string
   description = "API key to authenticate to StackGuardian"
-  default = "sgu_6366Rj2tDHhoAf6M5zYA9"
 }
 variable "org_name" {
   type = string
   description = "Organisation name in StackGuardian platform"
-  default = "wicked-hop"
+}
+variable "ResourceName" {
+  type = string
+  description = "name of the workflow group"
 }
