c68ba02d250890dc2b94b9e640d0d3dee1c66cd1
Digbijayinee Clara <digbijayineeclara@Digbijayinees-MacBook-Pro.local>
2024-08-07 16:17:12 +0530

    stackguardian-workflow-group-terraform-changes-1st-commit

--- stackguardian_workflow_group/variables.tf
+++ stackguardian_workflow_group/variables.tf
@@ -1,8 +1,10 @@
 variable "api_key" {
   type = string
   description = "API key to authenticate to StackGuardian"
+  default = "sgu_6366Rj2tDHhoAf6M5zYA9"
 }
 variable "org_name" {
   type = string
   description = "Organisation name in StackGuardian platform"
+  default = "wicked-hop"
 }
