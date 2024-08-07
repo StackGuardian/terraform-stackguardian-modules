d8cd835a93f2f31de9c676f69667c46e6eccd0d9
Digbijayinee Clara <digbijayineeclara@Digbijayinees-MacBook-Pro.local>
2024-08-07 14:41:25 +0530

    stackguardian-workflow-group-terraform-changes-1st-commit

---
+++ stackguardian_workflow_group/variables.tf
@@ -0,0 +1,8 @@
+variable "api_key" {
+  type = string
+  description = "API key to authenticate to StackGuardian"
+}
+variable "org_name" {
+  type = string
+  description = "Organisation name in StackGuardian platform"
+}
