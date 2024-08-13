3e55788314f5c8569c8a428a95c261d25953c5f9
Digbijayinee Clara <52991229+DigbijayineeClara07@users.noreply.github.com>
2024-08-13 11:39:31 +0530

    Update main.tf

--- stackguardian_workflow_group/main.tf
+++ stackguardian_workflow_group/main.tf
@@ -1,6 +1,6 @@
 resource "stackguardian_workflow_group" "Example-WorkflowGroup" {
   data = jsonencode({
-    "ResourceName" : var.ResourceName,
+    "ResourceName" : var.workflow_group_name,
     "Description" : "Example of terraform-provider-stackguardian for WorkflowGroup",
     "Tags" : ["tf-provider-example"],
     "IsActive" : 1,
