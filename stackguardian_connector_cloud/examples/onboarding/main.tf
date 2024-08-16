950d6e00cce3061b413309f50f2a5b324f2039be
Johannes Scheuerer <joscheuerer@gmail.com>
2024-08-16 16:13:08 +0200

    Update main.tf

--- stackguardian_connector_cloud/examples/onboarding/main.tf
+++ stackguardian_connector_cloud/examples/onboarding/main.tf
@@ -3,7 +3,7 @@ module "stackguardian_connector_cloud" {
     api_key = var.api_key
     connector = var.connector
     org_name = var.org_name
-    awsaccesskey = var.awsaccesskey
+    awsaccesskeyid = var.awsaccesskeyid
     awssecretaccesskey = var.awssecretaccesskey
     region = var.region
 }
