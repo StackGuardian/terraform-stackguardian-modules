c60d707b2c9d4c9c56eafd8d08804ff1668c71df
Jo Scheuerer <joscheuerer@gmail.com>
2024-08-18 19:12:47 +0200

    update vars

--- stackguardian_role/examples/onboarding/variables.tf
+++ stackguardian_role/examples/onboarding/variables.tf
@@ -10,13 +10,6 @@ variable "org_name" {
   type = string
   default = ""
 }
-variable "allowed_permissions" {
-  type = any
-  default = {
-      "Permission-key-1" : "Permission-val-1",
-      "Permission-key-2" : "Permission-val-2"
-    }
-}
 variable "workflow_groups" {
   type = list
   default = ["group-dev", "group-prod"]
