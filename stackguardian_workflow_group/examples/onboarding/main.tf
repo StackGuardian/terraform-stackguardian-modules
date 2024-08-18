1f8ee6cc06cc960cad73b1c2eb536b2c39ea6cea
Johannes Scheuerer <joscheuerer@gmail.com>
2024-08-19 00:11:34 +0200

    Update main.tf

--- stackguardian_workflow_group/examples/onboarding/main.tf
+++ stackguardian_workflow_group/examples/onboarding/main.tf
@@ -1,7 +1,5 @@
 module "stackguardian_workflow_group" {
     for_each = toset(var.workflow_groups)
     source = "../.."
-    api_key = var.api_key
     workflow_group_name = each.key
-    org_name = var.org_name
 }
