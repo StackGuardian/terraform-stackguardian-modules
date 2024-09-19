cc3e9f20dc58f4de1ac392e7f2c73619011fb506
Jo Scheuerer <joscheuerer@gmail.com>
2024-09-19 18:22:35 +0200

    org_name

--- stackguardian_user/main.tf
+++ stackguardian_user/main.tf
@@ -1,12 +1,12 @@
 resource "null_resource" "test" {
   provisioner "local-exec" {
     command = <<EOT
-    curl --location 'https://api.app.stackguardian.io/api/v1/orgs/wicked-hop/invite_user/' \
+    curl --location 'https://api.app.stackguardian.io/api/v1/orgs/${org_name}/invite_user/' \
 --header 'Content-Type: application/json' \
---header 'Authorization: apikey sgu_qJ3xuMzPCO9S21u7o2XOZ' \
+--header 'Authorization: apikey ${var.api_key}' \
 --data '{
-  "userId": "ttt/thisismygroup",
-  "role": "test"
+  "userId": "${var.group_or_user}",
+  "role": "${var.group_or_user}"
 }'
     EOT
   }
--- stackguardian_user/variables.tf
+++ stackguardian_user/variables.tf
@@ -6,7 +6,11 @@ variable "api_key" {
   type = string
   description = "API key to authenticate to StackGuardian"
 }
-variable "user" {
+variable "group_or_user" {
   type = string
-  description = "API key to authenticate to StackGuardian"
+  description = "Group or User that should be onboarded"
+}
+variable "role_name" {
+  type = string
+  description = "Role that should be assigned to the User or group"
 }
