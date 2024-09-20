07fc160d95e305912a25806d166a26ef6ea8e304
Jo Scheuerer <joscheuerer@gmail.com>
2024-09-20 09:46:16 +0200

    change to tf data

--- stackguardian_user/main.tf
+++ stackguardian_user/main.tf
@@ -1,13 +1,14 @@
-resource "null_resource" "test" {
-  provisioner "local-exec" {
-    command = <<EOT
-    curl --location 'https://api.app.stackguardian.io/api/v1/orgs/${var.org_name}/invite_user/' \
---header 'Content-Type: application/json' \
---header 'Authorization: apikey ${var.api_key}' \
---data '{
-  "userId": "${var.group_or_user}",
-  "role": "${var.role_name}"
-}'
-    EOT
+data "http" "invite_user" {
+  url    = "https://api.app.stackguardian.io/api/v1/orgs/${var.org_name}/invite_user/"
+  method = "POST"
+
+  request_headers = {
+    Content-Type = "application/json"
+    Authorization = "apikey ${var.api_key}"
   }
+
+  request_body = jsonencode({
+    userId        = var.group_or_user
+    role          = var.role_name
+  })
 }
