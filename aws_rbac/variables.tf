7582283e35111fc3a47d2657aa34a6db52d89668
Johannes Scheuerer <johannes.scheuerer@stackguardian.io>
2025-01-19 17:15:39 +0100

    Update variables.tf

--- aws_rbac/variables.tf
+++ aws_rbac/variables.tf
@@ -1,11 +1,22 @@
-variable "name" {
+variable "aws_region" {
   type = string
+  default = "eu-central-1"
+  description = "AWS region"
 }
 
-variable "orgname" {
+variable "aws_role_name" {
   type = string
+  description = "Name of the Role within AWS" 
 }
 
-variable "policy_name" {
+variable "role_external_id" {
   type = string
+  # Example 'my-stackguardian-org:abc12345'
+  description = "ExternalID for the Role in AWS - needs to start with the StackGuardian Organisation name."
+}
+
+variable "aws_policy" {
+  type = string
+  description = "ARN of the AWS Policy to be applied"
+  default= "arn:aws:iam::aws:policy/ReadOnlyAccess"
 }
