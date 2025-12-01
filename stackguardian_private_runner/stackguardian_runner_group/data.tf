6140a69e9caf0f18f3a87e48418c7931279e3ee4
Adis Halilović <adis.halilovic@stackguardian.io>
2025-12-01 01:36:20 +0100

    SG-3410: Fix runner_group_id key name.

--- stackguardian_private_runner/stackguardian_runner_group/data.tf
+++ stackguardian_private_runner/stackguardian_runner_group/data.tf
@@ -9,14 +9,14 @@ data "stackguardian_runner_group" "existing" {
 data "stackguardian_runner_group_token" "existing" {
   count = var.mode == "existing" ? 1 : 0
 
-  resource_name = var.existing_runner_group_name
+  runner_group_id = var.existing_runner_group_name
 }
 
 # Data source for token when creating new runner group
 data "stackguardian_runner_group_token" "created" {
   count = var.mode == "create" ? 1 : 0
 
-  resource_name = stackguardian_runner_group.this[0].resource_name
+  runner_group_id = stackguardian_runner_group.this[0].resource_name
 
   depends_on = [stackguardian_runner_group.this]
 }
